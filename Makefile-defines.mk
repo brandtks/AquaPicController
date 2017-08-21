88e02297b9b68550513bff5e30c44dc5d2d6fdfe
Skyler Brandt <brandtsa@live.com>
2017-08-21 00:46:15 -0400

    simplied building on dev end

--- Makefile-defines.mk
+++ Makefile-defines.mk
@@ -1,8 +1,8 @@
-NUGET="mono /path/to/nuget.exe"
-#NUGET="nuget"
-
 BUILD="Release"
 #BUILD="Debug" 
+
+NUGET="mono /path/to/nuget.exe"
+#NUGET="nuget"
 
 # This variable isn't required, just shortens the execution time of the build script
 # The script will try to find MSBuild and if it can't find it, it will download
