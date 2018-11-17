581d3a85f0d2d7ee6425e771228b2ce455cc432b
Skyler Brandt <brandtsa@live.com>
2018-11-17 14:12:02 -0500

    added some shading, its only okay at this point

--- TouchWidget/TouchCurvedProgressBar.cs
+++ TouchWidget/TouchCurvedProgressBar.cs
@@ -36,18 +36,14 @@ namespace GoodtimeDevelopment.TouchWidget
 
     public class TouchCurvedProgressBar : EventBox
     {
-        private float _progress;
+        float _progress;
         public float progress {
             get {
                 return _progress;
             }
             set {
-                if (value < 0.0f)
-                    _progress = 0.0f;
-                else if (value > 1.0f)
-                    _progress = 1.0f;
-                else
-                    _progress = value;
+                _progress = value;
+                _progress.Constrain (0, 1);
             }
         }
 
@@ -62,38 +58,39 @@ namespace GoodtimeDevelopment.TouchWidget
             TouchColor colorProgress,
             float currentProgress
         ) {
-            this.Visible = true;
-            this.VisibleWindow = false;
+            Visible = true;
+            VisibleWindow = false;
 
-            this.backgroundColor = colorBackground;
-            this.progressColor = colorProgress;
-            this._progress = currentProgress;
+            backgroundColor = colorBackground;
+            progressColor = colorProgress;
+            _progress = currentProgress;
             barWidth = 20;
             curveStyle = CurveStyle.HalfCurve;
 
             SetSizeRequest (160, 80);
 
-            this.ExposeEvent += OnExpose;
+            ExposeEvent += OnExpose;
         }
 
         public TouchCurvedProgressBar ()
-            : this (new TouchColor ("grey4"), new TouchColor ("pri"), 0.0f) {
+            : this (new TouchColor ("grey4"), new TouchColor ("pri"), 0) {
         }
 
         protected void OnExpose (object sender, ExposeEventArgs args) {
             using (Context cr = Gdk.CairoHelper.Create (GdkWindow)) {
-                int x = Allocation.Left;
-                int width = Allocation.Width;
-                int height = Allocation.Height;
-                int radius = barWidth / 2;
+                var left = Allocation.Left;
+                var top = Allocation.Top;
+                var width = Allocation.Width;
+                var height = Allocation.Height;
+                var radius = barWidth / 2;
                 int bigRadius;
 
                 double fudgeFactor = -0.0125 * radius;
 
                 if (curveStyle == CurveStyle.HalfCurve) {
                     bigRadius = height - radius - 2;
-                    int originX = x + (width / 2);
-                    int originY = Allocation.Top + height - radius;
+                    int originX = left + (width / 2);
+                    int originY = top + height - radius;
 
                     cr.MoveTo (originX - bigRadius + barWidth, originY);
                     cr.Arc (originX - bigRadius + radius, originY, radius, 0, Math.PI);
@@ -113,15 +110,15 @@ namespace GoodtimeDevelopment.TouchWidget
                     cr.Arc (originX, originY, bigRadius, Math.PI, r);
                     cr.Arc (x2, y2, radius, r, r + Math.PI);
                     cr.ArcNegative (originX, originY, bigRadius - barWidth + fudgeFactor, r, Math.PI);
-                    cr.LineTo (x, originY);
+                    cr.LineTo (left, originY);
                     cr.ClosePath ();
                     progressColor.SetSource (cr);
                     cr.Fill ();
                 } else if (curveStyle == CurveStyle.ThreeQuarterCurve) {
                     int below = (0.45 * height).ToInt ();
                     bigRadius = height - below - 2;
-                    int originX = x + (width / 2);
-                    int originY = Allocation.Top + height - below;
+                    int originX = left + (width / 2);
+                    int originY = top + height - below;
 
                     double x2 = TouchGlobal.CalcX (originX, bigRadius - radius, 3 * Math.PI / 4);
                     double y2 = TouchGlobal.CalcY (originY, bigRadius - radius, 3 * Math.PI / 4);
@@ -148,8 +145,27 @@ namespace GoodtimeDevelopment.TouchWidget
                     cr.Arc (x2, y2, radius, r, r + Math.PI);
                     cr.ArcNegative (originX, originY, bigRadius - barWidth + fudgeFactor, r, 3 * Math.PI / 4);
                     cr.ClosePath ();
-                    progressColor.SetSource (cr);
-                    cr.Fill ();
+
+                    var outlineColor = new TouchColor (progressColor);
+                    outlineColor.ModifyColor (0.5);
+                    var highlightColor = new TouchColor (progressColor);
+                    highlightColor.ModifyColor (1.4);
+                    var lowlightColor = new TouchColor (progressColor);
+                    lowlightColor.ModifyColor (0.75);
+
+                    var colorOriginX = TouchGlobal.CalcX (originX, bigRadius - 5, 11 * Math.PI / 8);
+                    var colorOriginY = TouchGlobal.CalcY (originY, bigRadius - 5, 11 * Math.PI / 8);
+                    using (var grad = new RadialGradient (colorOriginX, colorOriginY, 0, colorOriginX, colorOriginY, (bigRadius - 5) * 2)) {
+                        grad.AddColorStop (0, highlightColor.ToCairoColor ());
+                        grad.AddColorStop (0.35, progressColor.ToCairoColor ());
+                        grad.AddColorStop (0.75, lowlightColor.ToCairoColor ());
+                        cr.SetSource (grad);
+                        cr.FillPreserve ();
+                    }
+
+                    outlineColor.SetSource (cr);
+                    cr.LineWidth = 1;
+                    cr.Stroke ();
                 }
             }
         }
