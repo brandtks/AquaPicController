3e7c56ae65d81a8d7ebe755e66b028641b1dda40
Skyler Brandt <brandtsa@live.com>
2018-12-06 18:12:22 -0500

    renamed and moved home widget settings files to avoid continueing to confuse what file did what

---
+++ UserInterface/Scenes/Home/Dialogs/HomeWidgetDialogDomain/HomeSettingsWidget.cs
@@ -0,0 +1,158 @@
+#region License
+
+/*
+    AquaPic Main Control - Handles all functionality for the AquaPic aquarium controller.
+
+    Copyright (c) 2017 Goodtime Development
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/
+*/
+
+#endregion // License
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Cairo;
+using Gtk;
+using AquaPic.Runtime;
+using AquaPic.Globals;
+using GoodtimeDevelopment.TouchWidget;
+
+namespace AquaPic.UserInterface
+{
+    partial class HomeSettingsDialog
+    {
+        class HomeSettingsWidget
+        {
+            public string name;
+            public string group;
+            public string type;
+            HomeWidgetPlacement placement;
+
+            public RowColumnPair[] pairs {
+                get {
+                    return placement.ToRowColumnPairs ();
+                }
+            }
+
+            public int rowOrigin {
+                get {
+                    return placement.rowOrigin;
+                }
+                set {
+                    placement.rowOrigin = value;
+                }
+            }
+
+            public int columnOrigin {
+                get {
+                    return placement.columnOrigin;
+                }
+                set {
+                    placement.columnOrigin = value;
+                }
+            }
+
+            public int width {
+                get {
+                    return placement.width;
+                }
+            }
+
+            public int height {
+                get {
+                    return placement.height;
+                }
+            }
+
+            public string fullName {
+                get {
+                    return string.Format ("{0} {1} ({2})", name, group, type);
+                }
+            }
+
+            public HomeSettingsWidget (string name, string group, string type, int row, int column) {
+                this.name = name;
+                this.group = group;
+                this.type = type;
+                placement = new HomeWidgetPlacement (row, column);
+
+                switch (type) {
+                case "Timer": {
+                        placement.width = 3;
+                        placement.height = 2;
+                        break;
+                    }
+                case "LinePlot": {
+                        placement.width = 3;
+                        placement.height = 1;
+                        break;
+                    }
+                case "BarPlot": {
+                        placement.width = 1;
+                        placement.height = 2;
+                        break;
+                    }
+                case "CurvedBarPlot": {
+                        placement.width = 2;
+                        placement.height = 2;
+                        break;
+                    }
+                case "Button": {
+                        placement.width = 1;
+                        placement.height = 1;
+                        break;
+                    }
+                default:
+                    break;
+                }
+            }
+        }
+
+        private class HomeWidgetPlacement
+        {
+            public int rowOrigin;
+            public int columnOrigin;
+            public int width;
+            public int height;
+
+            public HomeWidgetPlacement (int rowOrigin, int columnOrigin) : this (rowOrigin, columnOrigin, 1, 1) { }
+
+            public HomeWidgetPlacement (int rowOrigin, int columnOrigin, int width, int height) {
+                this.rowOrigin = rowOrigin;
+                this.columnOrigin = columnOrigin;
+                this.width = width;
+                this.height = height;
+            }
+
+            public RowColumnPair[] ToRowColumnPairs () {
+                var pairs = new RowColumnPair[width * height];
+                for (int i = 0; i < pairs.Length; ++i) {
+                    pairs[i] = new RowColumnPair ();
+                    if (height != 1) {
+                        pairs[i].row = rowOrigin + (i / (pairs.Length / height));
+                    } else {
+                        pairs[i].row = rowOrigin;
+                    }
+                    pairs[i].column = columnOrigin + (i % width);
+                }
+                return pairs;
+            }
+        }
+    }
+}
+
---
+++ UserInterface/Scenes/Home/Dialogs/HomeWidgetDialogDomain/TileBoard.cs
@@ -0,0 +1,161 @@
+#region License
+
+/*
+    AquaPic Main Control - Handles all functionality for the AquaPic aquarium controller.
+
+    Copyright (c) 2017 Goodtime Development
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/
+*/
+
+#endregion // License
+
+using System;
+using Cairo;
+using Gtk;
+using GoodtimeDevelopment.TouchWidget;
+
+namespace AquaPic.UserInterface
+{
+    partial class HomeSettingsDialog
+    {
+        private class TileBoard : EventBox
+        {
+            Tile[,] tiles;
+
+            public bool containsNoConflictTiles {
+                get {
+                    foreach (var tile in tiles) {
+                        if (tile.status == TileStatus.Conflict) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+
+            public TileBoard () {
+                VisibleWindow = false;
+                Visible = true;
+
+                SetSizeRequest (222, 158);
+                tiles = new Tile[5, 7];
+                for (int row = 0; row < 5; ++row) {
+                    for (int column = 0; column < 7; ++column) {
+                        tiles[row, column] = new Tile ();
+                    }
+                }
+                ExposeEvent += OnExpose;
+            }
+
+            public void OccupyTile (HomeSettingsWidget widget) {
+                RowColumnPair[] pairs = widget.pairs;
+                foreach (var pair in pairs) {
+                    if (tiles[pair.row, pair.column].status == TileStatus.Free) {
+                        tiles[pair.row, pair.column].status = TileStatus.Occupied;
+                    } else if (tiles[pair.row, pair.column].status == TileStatus.Occupied) {
+                        tiles[pair.row, pair.column].status = TileStatus.Conflict;
+                    } else {
+                        throw new Exception ("Something happened freeing tiles on home screen settings");
+                    }
+                }
+            }
+
+            public void FreeTile (HomeSettingsWidget widget) {
+                RowColumnPair[] pairs = widget.pairs;
+                foreach (var pair in pairs) {
+                    if (tiles[pair.row, pair.column].status == TileStatus.Conflict) {
+                        tiles[pair.row, pair.column].status = TileStatus.Occupied;
+                    } else {
+                        tiles[pair.row, pair.column].status = TileStatus.Free;
+                    }
+                }
+            }
+
+            public void HighlightTile (HomeSettingsWidget widget) {
+                UnhighlightTile ();
+
+                RowColumnPair[] pairs = widget.pairs;
+                foreach (var pair in pairs) {
+                    if (tiles[pair.row, pair.column].status == TileStatus.Occupied) {
+                        tiles[pair.row, pair.column].status = TileStatus.Highlighted;
+                    }
+                }
+            }
+
+            public void UnhighlightTile () {
+                foreach (var tile in tiles) {
+                    if (tile.status == TileStatus.Highlighted) {
+                        tile.status = TileStatus.Occupied;
+                    }
+                }
+            }
+
+            protected void OnExpose (object sender, ExposeEventArgs args) {
+                using (Context cr = Gdk.CairoHelper.Create (GdkWindow)) {
+                    for (int row = 0; row < 5; ++row) {
+                        for (int column = 0; column < 7; ++column) {
+                            cr.Rectangle (column * 32 + Allocation.Left, row * 32 + Allocation.Top, 30, 30);
+                            switch (tiles[row, column].status) {
+                            case TileStatus.Occupied:
+                                TouchColor.SetSource (cr, "pri");
+                                break;
+                            case TileStatus.Highlighted:
+                                TouchColor.SetSource (cr, "secb");
+                                break;
+                            case TileStatus.Conflict:
+                                TouchColor.SetSource (cr, "compl");
+                                break;
+                            default:
+                                TouchColor.SetSource (cr, "grey4");
+                                break;
+                            }
+
+                            cr.Fill ();
+                        }
+                    }
+                }
+            }
+        }
+
+        enum TileStatus
+        {
+            Free,
+            Occupied,
+            Highlighted,
+            Conflict
+        }
+
+        private class Tile
+        {
+            public TileStatus status;
+
+            public Tile () {
+                status = TileStatus.Free;
+            }
+        }
+
+        private class RowColumnPair
+        {
+            public int row;
+            public int column;
+
+            public RowColumnPair () {
+                row = 0;
+                column = 0;
+            }
+        }
+    }
+}
+
