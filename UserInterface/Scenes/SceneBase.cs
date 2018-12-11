86d56ebdcfc3e22bbd58f4b0916c64f624c941ed
Skyler Brandt <brandtsa@live.com>
2018-12-11 08:59:00 -0500

    removed commented out section

--- UserInterface/Scenes/SceneBase.cs
+++ UserInterface/Scenes/SceneBase.cs
@@ -67,22 +67,7 @@ namespace AquaPic.UserInterface
 
             if (addTimer) {
                 timerId = GLib.Timeout.Add (1000, OnUpdateTimer);
-            }
-
-            //            Gdk.Pixbuf display = new Gdk.Pixbuf("images/background2.jpg");
-            //            string bpath = "temp", tempname = "temp";
-            //            for (int i = 0; File.Exists (tempname); i++)
-            //              tempname = bpath + i.ToString ();
-            //            display.Save (tempname, "png");
-            //            background = new Image (tempname);
-            //            Put (background, 0, 0);
-            //            background.Show ();
-            //            File.Delete (tempname);
-            //            Gdk.Pixbuf pic = new Gdk.Pixbuf ("images/background3.png");
-            //            this.background = new Image (pic);
-            //            this.Put (background, 0, 0);
-            //            this.background.Show ();
-            //            pic.Dispose ();
+            }          
         }
 
         protected virtual bool OnUpdateTimer () {
